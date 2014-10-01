fd3abcdcefe4a9b862bbf9bf880dbf944f20a220
Dominik Plíšek <dominik.plisek@plech.org>
2014-10-01 14:12:23 +0200

    fixed problem where finding a solution would mess up the board by not setting "previousState", + refactoring

--- PEK/main.c
+++ PEK/main.c
@@ -36,23 +36,23 @@ typedef struct {
 
 #pragma mark - Global variables
 
-#pragma mark Input
+#pragma mark - Input
 
 int *gameBoard;
 int gameBoardFieldCount;
 int gameBoardRows;
 int maxDepth;
 
-#pragma mark Output
+#pragma mark - Output
 
 StateStack* result;
 int minDepth = INT_MAX;
 
-#pragma mark Runtime
+#pragma mark - Runtime
 
 StateStack* stateStack;
 
-#pragma mark - Support functions
+#pragma mark - Mapping
 
 int fieldCountFromRows(int rows)
 {
@@ -63,6 +63,8 @@ int rowsFromFieldCount(int fieldCount)
 {
     return (-1 + sqrt(1 + (8 * fieldCount))) / 2;
 }
+
+#pragma mark - Logging
 
 void printGameBoard()
 {
@@ -89,6 +91,34 @@ void Log(const char *fmt, ...) {
     va_end(args);
 }
 
+#pragma mark - Startup
+
+BOOL loadGameBoardFromFileName(const char *fileName)
+{
+    FILE *file = fopen(fileName, "r");
+    if (file == NULL) {
+        printf("Input file with name %s not found.\n", fileName);
+        return NO;
+    }
+    int gameBoardFieldCountAllocated = GAME_BOARD_INITIAL_ALLOC;
+    gameBoard = (int *)malloc(sizeof(int) * gameBoardFieldCountAllocated);
+    int value;
+    fscanf(file, "%u", &value);
+    while (!feof(file)) {
+        if (gameBoardFieldCount + 1 > gameBoardFieldCountAllocated) {
+            int newAlloc = gameBoardFieldCountAllocated * gameBoardFieldCountAllocated;
+            gameBoard = realloc(gameBoard, sizeof(int) * newAlloc);
+            gameBoardFieldCountAllocated = newAlloc;
+        }
+        gameBoard[gameBoardFieldCount] = value;
+        gameBoardFieldCount++;
+        fscanf(file, "%u", &value);
+    }
+    fclose(file);
+    gameBoardRows = rowsFromFieldCount(gameBoardFieldCount);
+    return YES;
+}
+
 State *findInitialState()
 {
     for (int i = 0; i < gameBoardFieldCount; i++) {
@@ -104,33 +134,7 @@ State *findInitialState()
     return NULL;
 }
 
-BOOL loadGameBoardFromFileName(const char *fileName)
-{
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL) {
-        printf("File with name %s not found.\n", fileName);
-        return NO;
-    }
-    int gameBoardFieldCountAllocated = GAME_BOARD_INITIAL_ALLOC;
-    gameBoard = (int *)malloc(sizeof(int) * gameBoardFieldCountAllocated);
-    printf("Initially allocated %d ints for game board fields.\n", gameBoardFieldCountAllocated);
-    int value;
-    fscanf(file, "%u", &value);
-    while (!feof(file)) {
-        if (gameBoardFieldCount + 1 > gameBoardFieldCountAllocated) {
-            int newAlloc = gameBoardFieldCountAllocated * gameBoardFieldCountAllocated;
-            gameBoard = realloc(gameBoard, sizeof(int) * newAlloc);
-            gameBoardFieldCountAllocated = newAlloc;
-            printf("Reallocated %d ints for game board fields.\n", gameBoardFieldCountAllocated);
-        }
-        gameBoard[gameBoardFieldCount] = value;
-        gameBoardFieldCount++;
-        fscanf(file, "%u", &value);
-    }
-    fclose(file);
-    gameBoardRows = rowsFromFieldCount(gameBoardFieldCount);
-    return YES;
-}
+#pragma mark - State stack
 
 void pushState(State *state)
 {
@@ -156,6 +160,8 @@ State *popState()
     return state;
 }
 
+#pragma mark - Action
+
 void swapIndices(int i1, int i2)
 {
     int temp = gameBoard[i1];
@@ -163,13 +169,20 @@ void swapIndices(int i1, int i2)
     gameBoard[i2] = temp;
 }
 
-BOOL isFinal(State *state)
+BOOL backUpAndFindCommonParent(State *state, State *previousState)
 {
-    for (int i = 0; i < gameBoardFieldCount; i++) {
-        if (i != gameBoard[i]) return NO;
+    while (previousState != state->parent) {
+        if (!previousState->parent) {
+            printf("Reached top of state space while searching for common parent. That is an error.\n");
+            return NO;
+        }
+        swapIndices(previousState->blankIndex, previousState->parent->blankIndex);
+        previousState = previousState->parent;
     }
     return YES;
 }
+
+#pragma mark - Followup
 
 State *prepareFollowupState(State *state)
 {
@@ -179,78 +192,120 @@ State *prepareFollowupState(State *state)
     return prepared;
 }
 
+void pushLeftAndTopLeft(State* state, int row, int rowStart, int rowEnd)
+{
+    int blankIndex = state->blankIndex - 1;
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State* left = prepareFollowupState(state);
+        left->blankIndex = blankIndex;
+        pushState(left);
+    }
+    blankIndex = state->blankIndex - (rowEnd - rowStart + 1);
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State* topLeft = prepareFollowupState(state);
+        topLeft->blankIndex = blankIndex;
+        pushState(topLeft);
+    }
+}
+
+void pushRightAndTopRight(State* state, int row, int rowStart, int rowEnd)
+{
+    int blankIndex = state->blankIndex + 1;
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State *right = prepareFollowupState(state);
+        right->blankIndex = blankIndex;
+        pushState(right);
+    }
+    blankIndex = state->blankIndex - (rowEnd - rowStart);
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State *topRight = prepareFollowupState(state);
+        topRight->blankIndex = blankIndex;
+        pushState(topRight);
+    }
+}
+
+void pushBottomLeftAndBottomRight(State* state, int row, int rowStart, int rowEnd)
+{
+    int blankIndex = state->blankIndex + (rowEnd - rowStart + 1);
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State *bottomLeft = prepareFollowupState(state);
+        bottomLeft->blankIndex = blankIndex;
+        pushState(bottomLeft);
+    }
+    blankIndex = state->blankIndex + (rowEnd - rowStart + 2);
+    if (!state->parent || blankIndex != state->parent->blankIndex) {
+        State *bottomRight = prepareFollowupState(state);
+        bottomRight->blankIndex = blankIndex;
+        pushState(bottomRight);
+    }
+}
+
 void pushFollowupStates(State* state)
 {
     int row = rowsFromFieldCount(state->blankIndex);
     int rowStart = fieldCountFromRows(row);
     int rowEnd = fieldCountFromRows(row + 1) - 1;
-    printf("Going to push followup states.\n");
-    printf("row: %d, start: %d, end: %d\n", row, rowStart, rowEnd);
     if (state->blankIndex > rowStart) {
-        printf("Will push left and top left.\n");
-        int blankIndex = state->blankIndex - 1;
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State* left = prepareFollowupState(state);
-            left->blankIndex = blankIndex;
-            pushState(left);
-        }
-        blankIndex = state->blankIndex - (rowEnd - rowStart + 1);
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State* topLeft = prepareFollowupState(state);
-            topLeft->blankIndex = blankIndex;
-            pushState(topLeft);
-        }
+        pushLeftAndTopLeft(state, row, rowStart, rowEnd);
     }
     if (state->blankIndex < rowEnd) {
-        printf("Will push right and top right.\n");
-        int blankIndex = state->blankIndex + 1;
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State *right = prepareFollowupState(state);
-            right->blankIndex = blankIndex;
-            pushState(right);
-        }
-        blankIndex = state->blankIndex - (rowEnd - rowStart);
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State *topRight = prepareFollowupState(state);
-            topRight->blankIndex = blankIndex;
-            pushState(topRight);
-        }
+        pushRightAndTopRight(state, row, rowStart, rowEnd);
     }
     if (row < gameBoardRows - 1) {
-        printf("Will push bottom left and bottom right.\n");
-        int blankIndex = state->blankIndex + (rowEnd - rowStart + 1);
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State *bottomLeft = prepareFollowupState(state);
-            bottomLeft->blankIndex = blankIndex;
-            pushState(bottomLeft);
-        }
-        blankIndex = state->blankIndex + (rowEnd - rowStart + 2);
-        if (!state->parent || blankIndex != state->parent->blankIndex) {
-            State *bottomRight = prepareFollowupState(state);
-            bottomRight->blankIndex = blankIndex;
-            pushState(bottomRight);
-        }
+        pushBottomLeftAndBottomRight(state, row, rowStart, rowEnd);
     }
 }
 
-void saveResult(State *state)
+#pragma mark - Results
+
+BOOL isFinal(State *state)
 {
-    // save result
+    for (int i = 0; i < gameBoardFieldCount; i++) {
+        if (i != gameBoard[i]) return NO;
+    }
+    return YES;
+}
+
+BOOL saveResult(State *state, const char *fileName)
+{
+    FILE *file = fopen(fileName, "w");
+    if (file == NULL) {
+        printf("Output file with name %s not found.\n", fileName);
+        return NO;
+    }
+    
+    return YES;
+}
+
+BOOL saveResultIfBetter(State *state, const char *fileName)
+{
+    if (state->depth < minDepth) {
+        minDepth = state->depth;
+        return saveResult(state, fileName);
+    }
+    return YES;
+}
+
+#pragma mark - Support functions
+
+BOOL makesSenseToGoDeeper(State *state)
+{
+    return state->depth < maxDepth && state->depth < minDepth - 1;
 }
 
 #pragma mark - Main
 
 int main(int argc, const char * argv[])
 {
-    if (argc < 3) {
-        printf("Usage: programname <filename> <max depth>\n");
+    if (argc < 4) {
+        printf("Usage: programname <gameboard file> <max depth> <output file>\n");
         return EXIT_FAILURE;
     }
     const char *fileName = argv[1];
     if (!loadGameBoardFromFileName(fileName)) return EXIT_FAILURE;
     Log("Board loaded.");
     maxDepth = atoi(argv[2]);
-    printf("Max depth (from input) is %d.\n", maxDepth);
+    printf("Input max depth is %d.\n", maxDepth);
     State *initialState = findInitialState();
     if (!initialState) return EXIT_FAILURE;
     pushState(initialState);
@@ -258,34 +313,18 @@ int main(int argc, const char * argv[])
     while (stateStack->size) {
         State *state = popState();
         if (state->parent) {
-            printf("State has parent, is not root.\n");
-            while (previousState != state->parent) {
-                Log("Moved back up a notch.");
-                swapIndices(previousState->blankIndex, previousState->parent->blankIndex);
-                previousState = previousState->parent;
-                if (!previousState) {
-                    printf("Reached top of state space while searching for common parent. That is an error.\n");
-                    return EXIT_FAILURE;
-                }
-            }
+            if (!backUpAndFindCommonParent(state, previousState)) return EXIT_FAILURE;
             swapIndices(state->parent->blankIndex, state->blankIndex);
-            Log("Moved to the correct state.");
         }
-        if (isFinal(state)) {
-            printf("This state is final.\n");
-            if (state->depth < minDepth) {
-                printf("This state also has a smaller depth (%d) than the current %d, going to save it.\n", state->depth, minDepth);
-                minDepth = state->depth;
-                saveResult(state);
-            }
-            continue;
-        }
-        if (state->depth == maxDepth || state->depth == minDepth - 1) {
-            printf("Reached the max depth or the minDepth - 1, no need to go any deeper.\n");
-            continue;
-        }
-        pushFollowupStates(state);
         previousState = state;
+        if (isFinal(state)) {
+            saveResultIfBetter(state, argv[3]);
+            continue;
+        }
+        if (makesSenseToGoDeeper(state)) {
+            pushFollowupStates(state);
+        }
     }
+    printf("Program complete. The shortest solution has %d steps and has been saved to %s.\n", minDepth, argv[3]);
     return EXIT_SUCCESS;
 }
